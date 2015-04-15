8e58cdeab4083d1c14dd06b63fe6e482c55cf8dc
jroca72 <jroca72@gmail.com>
2015-04-15 11:47:07 +0200

    primera vista /principal

--- atorta/principal/views.py
+++ atorta/principal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from principal.models import Articulo
+
 # Create your views here.
+
+def portada(request):
+	los_articulos = Articulo.objects.all()
+	context = {'los_articulos': los_articulos }
+	return render(request, 'principal/index.html', context)
