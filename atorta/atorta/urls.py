d0f67f1dd845a5555f2c38dcb21e56a75b1b2c80
jroca72 <jroca72@gmail.com>
2015-04-20 21:10:46 +0200

    por fin con imagenes, cambios en settings y en carpetas de media e imagenes

--- atorta/atorta/urls.py
+++ atorta/atorta/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 
+
 urlpatterns = patterns('',
 	url(r'^imagenes/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': settings.MEDIA_ROOT,
@@ -10,3 +11,10 @@ urlpatterns = patterns('',
     url(r'^principal/', 'principal.views.portada', name = 'portada'),
     url(r'^sobre/', 'principal.views.sobre', name = 'sobre'),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
+
