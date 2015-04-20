cabad3ccef655d137e3f680bca202e5d8dad1ce9
jroca72 <jroca72@gmail.com>
2015-04-20 18:02:09 +0200

    todo va a su sitio poco a poco, pero de las imagenes nada

--- atorta/atorta/settings.py
+++ atorta/atorta/settings.py
@@ -80,7 +80,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
 
-MEDIA_URL = os.path.join(BASE_DIR,'/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, './templates').replace('\\', '/'),
+    )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, './imagenes')
+
+MEDIA_URL = "imagenes/"
+
--- atorta/atorta/urls.py
+++ atorta/atorta/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
+	url(r'^imagenes/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^principal/', 'principal.views.portada', name = 'portada'),
     url(r'^sobre/', 'principal.views.sobre', name = 'sobre'),
